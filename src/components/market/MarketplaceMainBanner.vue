86b6252dfb5f376cb233806e833eb7824690f239
Journey Park <tenderain@gmail.com>
2021-04-26 19:36:40 +0900

    change banners

--- src/components/market/MarketplaceMainBanner.vue
+++ src/components/market/MarketplaceMainBanner.vue
@@ -2,92 +2,90 @@
   <section class="market-home__banner">
     <div class="market-home__banner__item-container">
       <div
-          v-for="(item, i) in getBannerList"
-          :class="`market-home__banner__item idx-${i}`"
-          :data-idx="i"
-          @click="(event) => bannerClicked(event, item.link)"
+        v-for="(item, i) in getBannerList"
+        :class="`market-home__banner__item idx-${i}`"
+        :key="i"
+        :data-idx="i"
+        @click="(event) => bannerClicked(event, item.link)"
       >
         <div class="market-home__banner__item-content-container">
-          <div class="market-home__banner__item-content-title">{{item.title}}</div>
-          <div class="market-home__banner__item-content-description">
-            <div v-for="desc in item.description">{{ desc }}</div>
+          <!-- <div class="market-home__banner__item-content-title">
+            {{ item.title }}
           </div>
-          <div class="market-home__banner__item-content-link">{{item.link}}</div>
+          <div class="market-home__banner__item-content-description">
+            <div v-for="desc in item.description" :key="desc">{{ desc }}</div>
+          </div>
+          <div class="market-home__banner__item-content-link">
+            {{ item.link }}
+          </div> -->
         </div>
-        <img :src="$static.getFileURL(item.img.src)" >
+        <img :src="$static.getFileURL(item.img.src)" />
       </div>
     </div>
   </section>
 </template>
 
 <script>
-let $t, self
+let $t, self;
 
 export default {
-  name: 'MarketplaceMainBanner',
+  name: "MarketplaceMainBanner",
   created() {
-    self = this
-    $t = this.$t.bind(this)
+    self = this;
+    $t = this.$t.bind(this);
   },
 
-  mounted() {
+  mounted() {},
 
-  },
-
-  beforeDestroy() {
-
-  },
+  beforeDestroy() {},
 
   data() {
-    return {
-
-    }
+    return {};
   },
 
   computed: {
     getBannerList() {
       return [
         {
-          title: 'Axie',
-          description: ['Play to earn revolution', 'with Axie Infinity!'],
-          link: 'www.Infinityaxieinfinity.com',
+          title: "Phoenix Valley",
+          description: [
+            "The freedom and healing of",
+            "colors within the horse",
+          ],
+          link: "www.enftee.com",
           img: {
-            src: 'img/article/article-market-home-banner-axie.png'
-          }
+            src: "img/article/article-market-home-banner-phoenix.jpg",
+          },
         },
         {
-          title: 'JOYWORLD',
-          description: ['Welcome to JOYWORLD!', 'Become a JOY Collector.'],
-          link: 'www.joy.lol',
+          title: "K Museum Contemporary Art",
+          description: ["We Create, Present,", "And Do Magic"],
+          link: "www.enftee.com",
           img: {
-            src: 'img/article/article-market-home-banner-joyworld.png'
-          }
+            src: "img/article/article-market-home-banner-museum.jpg",
+          },
         },
         {
-          title: 'DozerBird',
-          description: ['DozerBird now live!', 'Test your tapping & timing!'],
-          link: 'dozerbird.io',
+          title: "TOPNFT.WORLD",
+          description: ["NFT Awards", "Since NFT Deserves fun"],
+          link: "www.topnft.world",
           img: {
-            src: 'img/article/article-market-home-banner-dozerbird.png'
-          }
+            src: "img/article/article-market-home-banner-topnft.jpg",
+          },
         },
-      ]
-    }
+      ];
+    },
   },
 
-  watch: {
-
-  },
+  watch: {},
 
   methods: {
     bannerClicked(event, link) {
       const targetURL = `http://${link}`;
       window.open(targetURL, `banner ${link}`);
-    }
+    },
   },
 
-  components: {
-
-  }
-}
+  components: {},
+};
 </script>
