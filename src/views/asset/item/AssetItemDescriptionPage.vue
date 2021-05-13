5d993455338efb7650ae7fe15a7f94c32dc8c96c
Jongsic <jongsic@ozys.net>
2021-05-13 12:34:51 +0900

    Release-1.19

--- src/views/asset/item/AssetItemDescriptionPage.vue
+++ src/views/asset/item/AssetItemDescriptionPage.vue
@@ -10,6 +10,16 @@
       </span>
     </div>
 
+    <div class="asset-item__hypen-row">
+      <span class="asset-item__hypen-row__label">
+        {{$t('Market.PublisherTitle')}}
+      </span>
+      <span class="divider"></span>
+      <span :class="'asset-item__hypen-row__value text-secondary' + (havePublisher ? ' item-publisher' : '')" @click="publisherClicked">
+        {{ getPublisherAddress }}
+      </span>
+    </div>
+
     <article class="asset-item__description__article">
       {{ info.metadata ? info.metadata.description : '' }}
     </article>
@@ -50,12 +60,49 @@
         if (info && info.isItemOnMyWallet) return $t('Market.OwnerIsMe');
 
         return '';
+      },
+
+      havePublisher() {
+        // TODO: 아래 주석 제거.
+        // if (작가 있으면) {
+        //   return true;
+        // }
+        // else if (작가 없으면) {
+        //   return false;
+        // }
+
+
+        // TODO: 위 주석 제거 시, 아래 문장 삭제.
+        return true;
+        //
+      },
+
+      getPublisherAddress() {
+        // TODO : 작가 이름을 이 곳에 넣을 것.
+        if (this.havePublisher) {
+          // TODO : 실제 데이터 입력 시, 작가 이름을 동적 변경.
+          // 홍길동과 미상은 더미 데이터.
+          return '홍길동';
+        }
+        else {
+          return '미상';
+        }
       }
     },
 
     watch: {},
 
-    methods: {},
+    methods: {
+      publisherClicked(event) {
+        if (!this.havePublisher) {
+          return;
+        }
+
+        // TODO: targetURL에 작가 정보 페이지 링크를 넣을 것.
+        const targetURL = "https://enftee.com/";
+        window.open(targetURL, `publisher ${targetURL}`);
+      }
+    },
 
     components: {}
   }
