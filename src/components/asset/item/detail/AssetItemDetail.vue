598d7b4d3f4f5a3e54bee672c524f9b1f8f0f1fe
maro <xzye123@naver.com>
2021-08-19 23:54:10 +0900

    detail tab commit

--- src/components/asset/item/detail/AssetItemDetail.vue
+++ src/components/asset/item/detail/AssetItemDetail.vue
@@ -18,8 +18,14 @@
       </ul>
     </nav>
 
-    <div class="asset-item__detail__body">
+    <div class="asset-item__detail__body m-dis-none">
       <router-view :info="info" :history="history" />
+    </div>
+    <div class="asset-item__detail__body p-dis-none">
+      <asset-item-description-page />
+    </div>
+    <div class="asset-item__detail__body p-dis-none">
+      <asset-item-collection-page />
     </div>
 
     <asset-item-floating :info="info">
@@ -48,6 +54,9 @@
 import AssetItemFloating from '@/components/asset/item/AssetItemFloating';
 import AssetItemDetailChart from '@/components/asset/item/detail/AssetItemDetailChart';
 import GlobalValueMixin from '@/mixins/common/GlobalValueMixin';
+
+import AssetItemDescriptionPage from '@/views/asset/item/AssetItemDescriptionPage';
+import AssetItemCollectionPage from '@/views/asset/item/AssetItemCollectionPage';
 
 let $t, component;
 
